4ea1a754d906c201334d4934f8852daba1b3cd06
Alexander Doroshenko <adoprog@users.noreply.github.com>
2022-05-22 21:27:12 -0400

    Updated @jsonstorage/personalize-react to 0.1.0

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,35 @@
 import { usePersonalization } from "@jsonstorage/personalize-react";
 
 const config = {
-  id: "5725bb31-8a05-4754-ba35-8f12024e78e4/fa0f4907-6c9a-4ba4-82ac-3658ff5ed760"
+  orgId: "5725bb31-8a05-4754-ba35-8f12024e78e4",
+  itemId: "fa0f4907-6c9a-4ba4-82ac-3658ff5ed760",
 };
 
 export const Header = (props) => {
   const variant = usePersonalization(config);
 
   return (
-    <header id='header'>
-      <div className={'intro ' + variant.data.style}>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
+    <header id="header">
+      <div className={"intro " + variant.data.style}>
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
                 <h1>
-                  { variant.data.title }
+                  {variant.data.title}
                   <span></span>
                 </h1>
-                <p>{ variant.data.text }</p>
+                <p>{variant.data.text}</p>
                 <a
-                  href='?persona=marketer'
-                  className='btn btn-custom btn-lg page-scroll'
+                  href="?persona=marketer"
+                  className="btn btn-custom btn-lg page-scroll"
                 >
                   Marketer
-                </a>{'  '}
+                </a>
+                {"  "}
                 <a
-                  href='?persona=developer'
-                  className='btn btn-custom btn-lg page-scroll'
+                  href="?persona=developer"
+                  className="btn btn-custom btn-lg page-scroll"
                 >
                   Developer
                 </a>
@@ -37,5 +39,5 @@ export const Header = (props) => {
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
